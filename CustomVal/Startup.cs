4dc57d8b3664ab4e0c27615ddb7498a6588004a8
Vishal Jaiswal <vjaisknit143@gmail.com>
2018-11-26 19:23:14 +0530

    Add project files.

---
+++ CustomVal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomVal.Startup))]
+namespace CustomVal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
